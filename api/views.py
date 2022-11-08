0718c77b6a63696d8dd6ef2731d7408e6e145527
wryu <useong3754@gmail.com>
2022-11-08 21:45:26 +0900

    페이지 이동 API

--- api/views.py
+++ api/views.py
@@ -8,7 +8,18 @@ from blog.models import Post, Category, Tag
 
 
 class ApiPostLV(BaseListView):
-    model = Post
+    # model = Post
+
+    def get_queryset(self):
+        paramCate = self.request.GET.get('category')
+        paramTag = self.request.GET.get('tag')
+        if paramCate:
+            qs = Post.objects.filter(category__name__iexact=paramCate)
+        elif paramTag:
+            qs = Post.objects.filter(tags__name__iexact=paramTag)
+        else:
+            qs = Post.objects.all()
+        return qs
 
     def render_to_response(self, context, **response_kwargs):
         qs = context['object_list']
