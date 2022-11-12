3776ba93dc4372e38fb600d98b5a63e3b03de6c0
wryu <useong3754@gmail.com>
2022-11-12 23:55:37 +0900

    ListView 수정
    장고에서 JS로 데이터를 직접보내 데이터 전송 횟수를 줄임

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,39 @@
-from django.views.generic import TemplateView
+import json
+
+from django.views.generic import TemplateView, ListView
+
+from api.utils import obj_to_post
+from blog.models import Post
+
+# class HomeView(TemplateView):
+#     template_name = 'home.html'
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    # model = Post
     template_name = 'home.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        paramCate = self.request.GET.get('category')
+        paramTag = self.request.GET.get('tag')
+        if paramCate:
+            qs = Post.objects.filter(category__name__iexact=paramCate)
+        elif paramTag:
+            qs = Post.objects.filter(tags__name__iexact=paramTag)
+        else:
+            qs = Post.objects.all()
+        return qs
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        postList = [obj_to_post(obj) for obj in context['object_list']]
+        pageCnt = context['paginator'].num_pages
+        curPage = context['page_obj'].number
+        dataDict = {
+            'postList': postList,
+            'pageCnt': pageCnt,
+            'curPage': curPage,
+        }
+        context['myJson'] = json.dumps(dataDict)
+        return context
