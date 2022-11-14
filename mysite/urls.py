f91c9c68e9f9d8732f8fd38a45047b5ec199e1b1
wryu <useong3754@gmail.com>
2022-11-14 19:50:19 +0900

    DRF 기본 세팅

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,21 +13,50 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib import admin
+# from django.urls import path, include
+#
+# from mysite.views import HomeView
+#
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     # blog
+#     path('', HomeView.as_view(), name='home'),
+#     path('blog/', include('blog.urls')),
+#     path('api/', include('api.urls')),
+#     # DRF
+#     path('api-auth/', include('rest_framework.urls')),
+# ]
+#
+# # static함수를 사용해서 settings파일의 MEDIA_URL에 작성한 URL이 들어오면
+# # MEDIA_ROOT에서 파일을 찾아서 처리한다.
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
-from mysite.views import HomeView
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
 
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='home'),
-    path('blog/', include('blog.urls')),
-    path('api/', include('api.urls')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
-
-# static함수를 사용해서 settings파일의 MEDIA_URL에 작성한 URL이 들어오면
-# MEDIA_ROOT에서 파일을 찾아서 처리한다.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
