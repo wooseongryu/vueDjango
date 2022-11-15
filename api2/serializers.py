e661f73084178f07cf0bcdaee909c11bb4226a55
wryu <useong3754@gmail.com>
2022-11-15 20:48:19 +0900

    좋아요 데이터 전송 기능

--- api2/serializers.py
+++ api2/serializers.py
@@ -28,3 +28,11 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
+
+class PostLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        # fields = '__all__'
+        fields = ['like']
+
