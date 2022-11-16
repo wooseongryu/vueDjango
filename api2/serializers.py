847edb7ccb6b8bccb77295c9380acf93771eb1c4
wryu <useong3754@gmail.com>
2022-11-16 23:57:53 +0900

    DRF RelatedField

--- api2/serializers.py
+++ api2/serializers.py
@@ -20,6 +20,9 @@ class PostListSerializer(serializers.ModelSerializer):
 
 
 class PostRetrieveSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    tags = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Post
         # fields = '__all__'
